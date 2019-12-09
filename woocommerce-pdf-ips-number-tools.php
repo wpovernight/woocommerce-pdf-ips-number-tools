ba18e123e3766415d7884857606665924fc8b1dc
Ewout Fernhout <info@drwoo.nl>
2019-12-09 10:17:37 +0100

    v1.1

--- woocommerce-pdf-ips-number-tools.php
+++ woocommerce-pdf-ips-number-tools.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce PDF Invoices & Packing Slips number tools
  * Plugin URI: http://www.wpovernight.com
  * Description: Provides debugging tools for invoice numbers
- * Version: 1.0
+ * Version: 1.1
  * Author: Ewout Fernhout
  * Author URI: http://www.wpovernight.com
  * License: GPLv2 or later
