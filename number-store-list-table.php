0a7d545674a1bd36ea159a6b2b0e3d498b1dc733
Yordan Soares <38109855+YordanSoares@users.noreply.github.com>
2021-10-22 14:46:46 -0400

    Add missing spaces to comply with WPCS

--- number-store-list-table.php
+++ number-store-list-table.php
@@ -158,8 +158,8 @@ class WPO_WCPDF_Number_Tools_List_Table extends \WP_List_Table {
 			'order'             => __( 'Order',            'woocommerce-pdf-ips-number-tools' ),
 			'order_status'      => __( 'Order Status',           'woocommerce-pdf-ips-number-tools' ),
 		);
-		if ( !isset( WPO_WCPDF()->settings->debug_settings['calculate_document_numbers'] ) ) {
-			unset($columns['calculated_number']);
+		if ( ! isset( WPO_WCPDF()->settings->debug_settings['calculate_document_numbers'] ) ) {
+			unset( $columns['calculated_number'] );
 		}
 		return apply_filters( 'wpo_wcpdf_number_tools_columns', $columns );
 	}
